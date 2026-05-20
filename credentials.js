dc6787548ff57db8cdfbffbd84d46a74ae788f50
EG12385 <61690459+EG12385@users.noreply.github.com>
2026-05-20 13:21:05 +0200

    Initial comit of project files

---
+++ credentials.js
@@ -0,0 +1,12 @@
+const authorizedUsers = {
+  "estelle": "Estelle2026!",
+  "rad":"rjBCP001",
+  "candice":"cmBCP002",
+  "ilze":"ijvvBCP003",
+  "hannes":"hvNetworks001",
+  "marius":"mkNetworks002",
+  "eugene":"esGlobe001",
+  "lucien":"lbGlobe002",
+
+
+};
---
+++ oxygen-webhelp/app/search/index/index-3.js
@@ -0,0 +1,2 @@
+// Auto generated index for searching.
+var index3 = {"services":"11*30*0$1","soon":"3*30*1$3","starts":"8*30*1$3","tips":"15*30*0$2","trade":"15*30*1$3","training":"16*30*0$2","us":"0*30*1$3","vision":"9*30*1$3","your":"6*30*1$4"};
